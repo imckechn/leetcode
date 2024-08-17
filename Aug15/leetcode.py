bb0c8b5d5baf2f7d6b509db11dc1caf7cab6a22d
Ian McKechnie <imckechn@uoguelph.ca>
2024-08-17 17:40:57 -0400

    Works when s % word[i] = 0 but not when it's none zerio :,(

--- Aug15/leetcode.py
+++ Aug15/leetcode.py
@@ -9,13 +9,28 @@ class Solution:
         while(i <= lenS - (iterationAmount * wordCount)):
             wordsDic = {}
             success = False
+            wordsFound = 0
             for j in range(i, i + iterationAmount * wordCount, iterationAmount):
                 success = True
                 wordBeingChecked = s[j:j + iterationAmount]
-                if wordBeingChecked in words and words.count(wordBeingChecked) > list(wordsDic.keys()).count(wordBeingChecked):
-                    wordsDic[wordBeingChecked] = j
+                if wordBeingChecked in words and (wordBeingChecked not in wordsDic.keys() or len(wordsDic[wordBeingChecked]) < words.count(wordBeingChecked)):
+                    wordsFound += 1
+                    if wordBeingChecked in wordsDic.keys() and words.count(wordBeingChecked) < len(wordsDic[wordBeingChecked]):
+                        success = False
+                        if wordBeingChecked not in words:
+                            i = j
 
-                    if wordCount == len(wordsDic):
+                        elif wordBeingChecked in wordsDic.keys():
+                            i = wordsDic[wordBeingChecked][0]
+
+                        break
+
+                    elif wordBeingChecked not in wordsDic.keys():
+                        wordsDic[wordBeingChecked] = []
+
+                    wordsDic[wordBeingChecked].append(j)
+
+                    if wordCount == wordsFound:
                         j = i
                         break
 
@@ -24,8 +39,8 @@ class Solution:
                     if wordBeingChecked not in words:
                         i = j
 
-                    elif ((wordBeingChecked in wordsDic.keys())):
-                        i = wordsDic[wordBeingChecked]
+                    elif wordBeingChecked in wordsDic.keys():
+                        i = wordsDic[wordBeingChecked][0]
 
                     break
                     
@@ -41,4 +56,5 @@ class Solution:
 # print(Solution.findSubstring('barfoothefoobarman', ['foo', 'bar'])) #[0,9]
 # print(Solution.findSubstring('wordgoodgoodgoodbestword', ["word","good","best","word"])) #[]
 # print(Solution.findSubstring('barfoofoobarthefoobarman', ["bar","foo","the"])) #[6, 9, 12]
-print(Solution.findSubstring('wordgoodgoodgoodbestword', ["word","good","best","good"])) #[8]
+# print(Solution.findSubstring('wordgoodgoodgoodbestword', ["word","good","best","good"])) #[8]
+print(Solution.findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake", ["fooo","barr","wing","ding","wing"])) #[13]

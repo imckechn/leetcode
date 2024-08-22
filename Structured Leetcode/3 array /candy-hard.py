6f40516137b7f1c3f301dcfcc8d44fc382e9b591
Ian McKechnie <imckechn@uoguelph.ca>
2024-08-22 07:59:08 -0400

    Second most optimal solution

--- Structured Leetcode/3 array /candy-hard.py
+++ Structured Leetcode/3 array /candy-hard.py
@@ -1,38 +1,23 @@
 class Solution:
-
-    def increasePreviousNumbers(candiesArr, ratings):
-        for i in range(len(candiesArr) -1, -1, -1):
-            candiesArr[i] = candiesArr[i] + 1
-            if i != 0:
-                if candiesArr[i-1] != candiesArr[i] or ratings[i] == ratings[i-1]:
-                    break
-
-
     def candy(ratings):
-        candiesArr = [1]
+        n = len(ratings)
+        candies = [1] * n 
 
-        for i in range (1, len(ratings)):
+        for i in range(1, n):
             if ratings[i] > ratings[i-1]:
-                candiesArr.append(candiesArr[i-1] + 1)
+                candies[i] = candies[i-1] + 1
 
-            if ratings[i] == ratings[i-1]:
-                candiesArr.append(1)
-
-            if ratings[i] < ratings[i-1]:
-                if candiesArr[i-1] == 1:
-                    Solution.increasePreviousNumbers(candiesArr, ratings)
-                    candiesArr.append(candiesArr[i-1] - 1)
-                else:
-                    candiesArr.append(1)
-
-        total = 0
-        for candy in candiesArr:
-            total += candy
-
-        return total
+        for i in range(n-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candies[i] = max(candies[i], candies[i+1] + 1)
+        
+        return sum(candies)
     
-print(Solution.candy([1,0,2])) #5
-print(Solution.candy([1,2,2])) #4
-print(Solution.candy([1,3,2,2,1])) #7
+        
+    
+    
+# print(Solution.candy([1,0,2])) #5
+# print(Solution.candy([1,2,2])) #4
+# print(Solution.candy([1,3,2,2,1])) #7
 print(Solution.candy([29,51,87,87,72,12])) #12
  

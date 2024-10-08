318c9885c0a14f6779f8155ebdea9be42c74d746
Ian McKechnie <imckechn@uoguelph.ca>
2024-10-08 22:24:03 +0100

    Soln found

--- Structured Leetcode/1 Matrix/spiralMatrix.py
+++ Structured Leetcode/1 Matrix/spiralMatrix.py
@@ -1,52 +1,55 @@
 class Solution:
     def spiralOrder(matrix):
-        output = []
+        output = [matrix[0][0]]
+        matrix[0][0] = None
         direction = 'Right'
-        x = 0
         y = 0
+        x = 0
+        maxY = len(matrix)
+        maxX = len(matrix[0])
+        
 
         while True:
-            if direction == 'Right':
-                if matrix[y][x+1] and matrix[y][x+1] != -1:
-                    output.append(matrix[y][x+1])
-                    matrix[y][x+1] = -1
+            if len(output) == maxX * maxY:
+                break
+
+            if direction == "Right":
+                if x + 1 != maxX and matrix[y][x+1] != None:
                     x += 1
                 else:
                     direction = "Down"
-
+                    continue
+            
             elif direction == "Down":
-                if matrix[y+1][x] and matrix[y+1][x] != -1:
-                    output.append(matrix[y+1][x])
-                    matrix[y+1][x] = -1
+                if y + 1 != maxY and matrix[y+1][x] != None:
+                    y += 1
                 else:
-                    direction = "left"
-                
-                y += 1
+                    direction = "Left"
+                    continue
 
-            elif direction == "left":
-                if matrix[y][x-1] and matrix[y][x-1] != -1:
-                    output.append(matrix[y][x-1])
-                    matrix[y][x-1] = -1
+            elif direction == "Left":
+                if x - 1 != -1 and matrix[y][x-1] != None:
+                    x -= 1
                 else:
-                    direction = "up"
-
-                x -= 1
-
-            elif direction == "up":
-                if matrix[y-1][x] and matrix[y-1][x] != -1:
-                    output.append(matrix[y-1][x])
-                    matrix[y-1][x] = -1
+                    direction = "Up"
+                    continue
+                    
+            else:
+                if y - 1 != -1 and matrix[y-1][x] != None:
+                    y -= 1
                 else:
-                    direction = "right"
+                    direction = "Right"
+                    continue
 
-                y -= 1
-
+            output.append(matrix[y][x])
+            matrix[y][x] = None
         
+        return output
 
 
-
-
-Solution.spiralOrder([[1,2,3],[4,5,6],[7,8,9]])
+print(Solution.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+print(Solution.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+print(Solution.spiralOrder([[2,5],[8,4],[0,-1]]))
 
 # [[1,2,3]
 #  [4,5,6]

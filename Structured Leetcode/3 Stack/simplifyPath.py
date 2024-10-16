d6b149e3db081c13e60d4c98b78329e0b72d36f6
Ian McKechnie <imckechn@uoguelph.ca>
2024-10-16 12:34:07 -0400

    much easier to read and faster

--- Structured Leetcode/3 Stack/simplifyPath.py
+++ Structured Leetcode/3 Stack/simplifyPath.py
@@ -1,33 +1,23 @@
 class Solution:
     def simplifyPath(path):
         dir = path.split("/")
+        ans = []
 
-        i = 0
-        output = "/"
-        while i < len(dir):
-            if dir[i] == "" or dir[i] == ".":
-                dir.pop(i)
-            elif dir[i] == ".." and i > 0:
-                dir.pop(i)
-                dir.pop(i-1)
-                i -= 1
-            elif dir[i] == "..":
-                dir.pop(i)
-            elif i+1 < len(dir) and dir[i+1] == "..":
-                dir.pop(i+1)
-                dir.pop(i)
+        for i in range(len(dir)):
+            if dir[i] != "." and dir[i] != "":
+                if dir[i] == "..":
+                    if len(ans) != 0:
+                        ans.pop(-1)
+                else:
+                    ans.append(dir[i])
 
-                if i != 0:
-                    i -= 1
-            else:
-                i += 1
+        if len(ans) == 0:
+            return "/"
 
         output = "/"
-        for elem in dir:
+        for elem in ans:
             output += elem + "/"
 
-        if output == "/":
-            output = "//"
         return output[:-1]
 
 # print(Solution.simplifyPath("/home/"))
@@ -35,7 +25,7 @@ class Solution:
 # print(Solution.simplifyPath("/home/home/home/home/"))
 # print(Solution.simplifyPath("/home/user/Documents/../Pictures"))
 # print(Solution.simplifyPath("/home/../Documents/../Pictures"))
-# print(Solution.simplifyPath("/../"))
+print(Solution.simplifyPath("/../"))
 # print(Solution.simplifyPath("/a/./b/../../c/"))
 # print(Solution.simplifyPath("/a/../../b/../c//.//"))
 # print(Solution.simplifyPath("/a//b////c/d//././/.."))

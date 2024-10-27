61de1fed7b9a7d5b18b5e0400f22bed0ba85fae8
Ian McKechnie <imckechn@uoguelph.ca>
2024-10-26 22:28:51 -0400

    Still got it, solved in N time, beats 100% of the competition

--- Structured Leetcode/4 Linked List/reverseLinkedListsII.c
+++ Structured Leetcode/4 Linked List/reverseLinkedListsII.c
@@ -9,39 +9,42 @@ struct ListNode {
 
 struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
     struct ListNode newHead;
-
     newHead.next = head;
+
     struct ListNode *cur = head;
     struct ListNode *prev = &newHead;
     struct ListNode *interlist;
     struct ListNode *interListTail;
 
-
+    int i = 0;
     while(true) {
+        i += 1; 
 
-        if (cur->val == left) {
+        if (interlist != 0) {
+            struct ListNode *next = cur->next;
+            cur->next = interlist;
+            interlist = cur;
+            cur = next;
+        }
+
+        if (i == left) {
             interlist = cur;
             interListTail = cur;
+            prev->next = 0;
+        }
 
-        } else if (cur->val == right) {
-            struct ListNode copy;
-            copy.val = cur->val;
-            copy.next = interlist;
-            interlist = &copy;
+        if (i == right) {
+            struct ListNode *next = cur->next;
+            cur->next = interlist;
+            interlist = cur;
 
             prev->next = interlist;
-            interListTail->next = cur->next;
+            interListTail->next = next;
             break;
-
-        } else if (interlist != 0) {
-            struct ListNode copy;
-            copy.val = cur->val;
-            copy.next = interlist;
-            interlist = &copy;
         }
-    
-        cur = cur->next;
+
         if (interlist == 0) {
+            cur = cur->next;
             prev = prev->next;
         }
         
@@ -70,8 +73,11 @@ int main() {
     head.val = 1;
     head.next = &d;
 
+    head.next = 0;
+
     // struct ListNode* head2 = reverseBetween(&head, 2, 4);
-    struct ListNode* head2 = reverseBetween(&head, 1, 3);
+    // struct ListNode* head3 = reverseBetween(&head, 1, 3);
+    struct ListNode* head2 = reverseBetween(&head, 1, 1);
 
     return 0;
 };

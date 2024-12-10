5d28829f06ea213d9d4dbd8d6eb50e1f26941ee7
Ian McKechnie <imckechn@uoguelph.ca>
2024-12-10 22:37:41 +0100

    Got it

--- Structured Leetcode/2 B-Tree BFS/levelOrderTraversal.py
+++ Structured Leetcode/2 B-Tree BFS/levelOrderTraversal.py
@@ -6,23 +6,40 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def visitNodes(root, queue, answers, level):
-        if root == None: answers
+    def visitNodes(root, queue, answers):
+        level = root[1]
+        node = root[0]
+        if node == None: 
+            if len(queue) == 0:
+                return []
+            else:
+                return Solution.visitNodes(queue.pop(0), queue, answers)
 
-        queue.append(root.left)
-        queue.append(root.right)
+        queue.append([node.left, level+1])
+        queue.append([node.right, level+1])
 
-        if level > len(answers):
+        if root[1] >= len(answers):
             answers.append([])
 
-        answers[level].append(root.val)
+        answers[root[1]].append(node.val)
 
-        return Solution.visitNodes(queue.pop(0), queue, answers, )
+        return Solution.visitNodes(queue.pop(0), queue, answers)
 
 
 
     def levelOrder(root):
         queue = []
         answers = []
-        queue, answers = Solution.visitNodes(root, queue, answers, 0)
+        Solution.visitNodes([root, 0], queue, answers)
+        return answers
         
+
+nodea = TreeNode(9)
+nodeb = TreeNode(15)
+nodec = TreeNode(7)
+noded = TreeNode(20, nodeb, nodec)
+root = TreeNode(3, nodea, noded)
+
+ans = Solution.levelOrder(root)
+
+print(ans)

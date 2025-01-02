9bcbad865f1097b480a03321a5fbfd950a48f7db
Ian McKechnie <imckechn@uoguelph.ca>
2025-01-02 18:17:42 -0500

    Loooool got it. O(N)

--- Structured Leetcode/1 BST/validBST.py
+++ Structured Leetcode/1 BST/validBST.py
@@ -7,38 +7,19 @@ class TreeNode:
 from typing import List, Optional
 
 class Solution:
-    def mustBeSmallerBST(self, root, min, max): # Left
+    def check(self, root, min, max):
         if root == None: 
             return True
-
-        if root.left != None and (root.left.val >= root.val or root.left.val >= max):
-            return False
-        elif root.right != None and (root.right.val <= root.val or root.right.val >= max) :
+        
+        if max != None and root.val >= max:
             return False
         
-        max = root.val
-        
-        a = Solution.mustBeLargerBST(self, root.left, min, max)
-        b = Solution.mustBeSmallerBST(self, root.right, min, max)
-
-        if not a or not b:
-            return False
-        return True
-    
-    def mustBeLargerBST(self, root, min, max): # Right
-        if root == None: 
-            return True
-
-        if root.left != None and (root.left.val >= root.val or root.left.val <= min):
-            return False
-        elif root.right != None and (root.right.val <= root.val or root.right.val <= min):
+        if min != None and root.val <= min:
             return False
         
-        max = root.val
+        a = Solution.check(self, root.left, min, root.val) #left
+        b = Solution.check(self, root.right, root.val, max) #right
         
-        a = Solution.mustBeLargerBST(self, root.left, min, max)
-        b = Solution.mustBeSmallerBST(self, root.right, min, max)
-
         if not a or not b:
             return False
         return True
@@ -46,56 +27,27 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         if root == None: 
             return True
-
-        if root.left != None and root.left.val >= root.val:
-            return False
-        elif root.right != None and root.right.val <= root.val:
-            return False
         
-        a = Solution.mustBeSmallerBST(self, root.left, root.val, root.val)
-        b = Solution.mustBeLargerBST(self, root.right, root.val, root.val)
+        a = Solution.check(self, root.left, None, root.val) #left
+        b = Solution.check(self, root.right, root.val, None) #right
 
         if not a or not b:
             return False
         return True
     
 
-nodea = TreeNode(1)
-nodeb = TreeNode(3)
-nodec = TreeNode(6)
-noded = TreeNode(4, nodeb, nodec)
-root = TreeNode(5, nodea, noded)
-
-nodea = TreeNode(0)
-nodeb = TreeNode(2)
-nodec = TreeNode(1, nodea, nodeb)
-
-nodea = TreeNode(4)
-nodeb = TreeNode(6)
-noded = TreeNode(5, nodea, nodeb)
-root = TreeNode(3, nodec, noded)
-
-
-
-nodec = TreeNode(4)
-
 nodea = TreeNode(3)
-nodeb = TreeNode(7)
-noded = TreeNode(6, nodea, nodeb)
-root = TreeNode(5, nodec, noded)
+
+nodeb = TreeNode(0)
+nodea = TreeNode(2, None, nodea)
+nodeb = TreeNode(1, nodeb, nodea)
+
+# node = TreeNode(4)
+# nodea = TreeNode(6)
+# node = TreeNode(5, node, nodea)
+node = TreeNode(3, nodeb)
 
 
-node = TreeNode(27)
-node = TreeNode(19, None, node)
-node = TreeNode(26, node)
-node2 = TreeNode(56)
-node2 = TreeNode(47, None, node2)
-node = TreeNode(32, node, node2)
 
-
-node = TreeNode(27)
-node = TreeNode(19, None, node)
-node = TreeNode(26, node)
-node = TreeNode(32, node)
 
 print(Solution.isValidBST(None, node))

9dc1fc4ae28adc9afa6395c05527cdecef1ad19d
Ian McKechnie <imckechn@uoguelph.ca>
2025-01-09 08:06:04 -0500

    solved, O(m*n)

--- Structured Leetcode/2 Graph General/numIslands.py
+++ Structured Leetcode/2 Graph General/numIslands.py
@@ -1,19 +1,36 @@
 from typing import List, Optional
 
 class Solution:
+    def search(grid, i, j):
+        grid[i][j] = "0"
+
+        if i + 1 < len(grid):
+            if grid[i+1][j] == "1":
+                Solution.search(grid, i+1, j)
+
+        if j + 1 < len(grid[0]):
+            if grid[i][j+1] == "1":
+                Solution.search(grid, i, j+1)
+
+        if i - 1 >= 0:
+            if grid[i-1][j] == "1":
+                Solution.search(grid, i-1, j)
+
+        if j - 1 >= 0:
+            if grid[i][j-1] == "1":
+                Solution.search(grid, i, j-1)
+
     def numIslands(grid: List[List[str]]) -> int:
         islands = 0
-        streak = False
-
+        
         for i in range(len(grid)):
-            for j in range(len(grid[i])):
+            for j in range(len(grid[0])):
                 if grid[i][j] == "1":
                     islands += 1
+                    Solution.search(grid, i, j)
 
-                    if i > 0 and grid[i-1][j] == "1":
-                        islands -= 1
-                    
         return islands
+
 
 grid = [
   ["1","1","1","1","0"],

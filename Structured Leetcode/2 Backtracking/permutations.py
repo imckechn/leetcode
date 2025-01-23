9b2d5b5f0b994aca00a6952cc096d33b2f224ba9
Ian McKechnie <imckechn@uoguelph.ca>
2025-01-23 08:26:08 -0500

    Making progress

---
+++ Structured Leetcode/2 Backtracking/permutations.py
@@ -0,0 +1,31 @@
+from typing import List, Optional
+
+class Solution:
+    def backTrack(nums, frozenIndex):
+        if frozenIndex+2 == len(nums):
+            return [nums.copy()]
+
+        solutions = []
+
+        for i in range(frozenIndex+1, len(nums)):
+            temp = nums[frozenIndex+1]
+            nums[frozenIndex+1] = nums[i]
+            nums[i] = temp
+
+            solutions += Solution.backTrack(nums, frozenIndex+1)
+        
+        return solutions
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        solutions = []
+
+        for i in range(len(nums)):
+            temp = nums[0]
+            nums[0] = nums[i]
+            nums[i] = temp
+
+            solutions += Solution.backTrack(nums, 0)
+        
+        return set(solutions)
+
+print(Solution.permute(None, [1,2,3])) 

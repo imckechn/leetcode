45c5dfce92eee35ba663b4af033419adb67afdd4
Ian McKechnie <imckechn@uoguelph.ca>
2025-01-25 12:17:00 -0500

    Success. N time as well as far as I can tell

--- Structured Leetcode/2 Divide & Conquer/sortList.py
+++ Structured Leetcode/2 Divide & Conquer/sortList.py
@@ -12,55 +12,70 @@ class Solution:
             return head
         return Solution.getLastNode(head.next)
     
-    def QS(head, last):
+    def QS(head, last): #Need to perform same action, but when at the last node, see if you need to swap instead of automatically swapping
         if last == head or head == None or last == None:
             return
 
-        pointerA = head
-        pointerB = head
+        newHead = ListNode(next=head)
 
-        while(pointerB != last):
+        if last == head:
+            return
+
+        pointerA = newHead
+        pointerB = newHead
+
+        while(True):
             pointerB = pointerB.next
 
             if pointerB.val < last.val:
-                
                 pointerA = pointerA.next
-                if pointerA.val > last.val:
+                pointerA.val, pointerB.val = pointerB.val, pointerA.val
+
+            elif pointerB.val == last.val:
+                pivot = pointerA
+                pointerA = pointerA.next
+
+                if pointerA.val > pointerB.val:
                     pointerA.val, pointerB.val = pointerB.val, pointerA.val
+                break
 
-        last, pointerA = pointerA, last
-
-        Solution.QS(head, pointerA)
-        Solution.QS(pointerA.next, last)
+        Solution.QS(head, pivot)
+        Solution.QS(pivot.next, last)
     
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]: #Need to run a quick sort algo on this
+        newHead = ListNode(next=head)
         last = Solution.getLastNode(head)
 
         if last == head:
             return
 
-        pointerA = head
-        pointerB = head
+        pointerA = newHead
+        pointerB = newHead
+        pivot = None
 
-        while(pointerB != last):
+        while(True):
             pointerB = pointerB.next
 
             if pointerB.val < last.val:
-                
                 pointerA = pointerA.next
-                if pointerA.val > last.val:
-                    pointerA.val, pointerB.val = pointerB.val, pointerA.val
+                pointerA.val, pointerB.val = pointerB.val, pointerA.val
+
+            elif pointerB.val == last.val:
+                pivot = pointerA
+                pointerA = pointerA.next
+                pointerA.val, pointerB.val = pointerB.val, pointerA.val
+                break
 
         last, pointerA = pointerA, last
 
-        Solution.QS(head, pointerA)
-        Solution.QS(pointerA.next, last)
+        Solution.QS(head, pivot)
+        Solution.QS(pivot.next, Solution.getLastNode(head))
 
-        return       
+        return
 
 nodea = ListNode(4)
 nodeb = ListNode(2, nodea)
-nodec = ListNode(3, nodeb)
-noded = ListNode(1, nodec)
+nodec = ListNode(1, nodeb)
+noded = ListNode(3, nodec)
 
 Solution.sortList(None, noded)

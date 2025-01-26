726020c5c5df1fa30bd5086dd0780815fa2d8d97
Ian McKechnie <imckechn@uoguelph.ca>
2025-01-26 10:05:59 -0500

    Got it

--- Structured Leetcode/3 Binary Search/2dMatrixSearch.py
+++ Structured Leetcode/3 Binary Search/2dMatrixSearch.py
@@ -10,24 +10,25 @@ class Solution:
             if matrix[i][0] > target:
                 return i-1
             
-        return False
+        return len(matrix)-1
 
     def findColumn(matrix, target, y):
         for i in range(len(matrix[y])):
             if matrix[y][i] == target:
                 return i
             
-        return False
+        return -1
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        y = Solution.findRow(matrix, target)
-
-        if not y:
-            return False
-        
+        y = Solution.findRow(matrix, target)        
         x = Solution.findColumn(matrix, target, y)
-        if not x:
+        if x == -1:
             return False
         
         return True
 
+print(Solution.searchMatrix(None, [[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+
+no = [  [1,3,5,7],
+        [10,11,16,20],
+        [23,30,34,60]]

39ed0a46ec79e249bef38265d9c51ef4a3a1ee17
Ian McKechnie <imckechn@uoguelph.ca>
2025-02-05 22:05:05 -0500

    So many one off errors im about to one off myself

--- Structured Leetcode/4 Multidimensional DP/longestPalindrome.py
+++ Structured Leetcode/4 Multidimensional DP/longestPalindrome.py
@@ -1,64 +1,83 @@
+import math
+
+
 class Solution:
     def getPalinRelectLen(s, a, b):
-        x = 1
+        x = 0
 
-        while a-x >= 0 and b+x+1 < len(s):
-            if s[a-x] != s[b+x+1]:
-                break
-            else:
-                x+=1
-
-        return x*2
-
-    def getPalinSiblingsLen(s, i):
-        x = 1
-
-        while i+x < len(s) and i-x >= 0:
-            if s[i-x] != s[i+x]:
-                break
+        while a-x >= 0 and b+x < len(s):
+            if s[a-x] != s[b+x]:
+                return x + b
             else:
                 x += 1
-        return x*2
+
+        return x + b - 1
+
+    def getPalinSiblingsLen(s, i):
+        x = 0
+
+        while i+x+1 < len(s) and i-x >= 0:
+            if s[i-x] != s[i+x+1]:
+                break
+            else:
+                x += 2
+        return x
 
     def longestPalindrome(self, s: str) -> str:
         if len(s) == 0:
-            return 0
+            return ""
         if len(s) == 1:
-            return 1
+            return s[0]
+        if len(s) == 2:
+            if s[0] == s[1]:
+                return s
+            else:
+                return s[0]
         
         largest = 1
         index = 0
         reflect = False
 
-        for i in range(len(s)-1):
-            if s[i] == s[i+1]:
-                ans = Solution.getPalinSiblingsLen(s, i)
-                if ans > largest: 
-                    largest = ans
-                    index = i
-                    
+        for i in range(1, len(s)):
 
-            elif s[i-1] == s[i+1]:
-                ans = Solution.getPalinRelectLen(s, i-1, i)
+            if i < len(s)-1 and s[i-1] == s[i+1]:
+                ans = Solution.getPalinRelectLen(s, i-1, i+1)
                 if ans > largest: 
                     largest = ans
                     index = i
                     reflect = True
 
+            if s[i-1] == s[i]:
+                ans = Solution.getPalinSiblingsLen(s, i-1)
+                if ans > largest: 
+                    largest = ans
+                    index = i
+                    reflect = False
+
         pal = ""
-
+    
         if reflect:
-            for i in range(index - largest//2, index + largest//2 +1):
+            for i in range(index - math.floor(largest/2), math.ceil(index + largest/2)):
                 pal += s[i]
+            
         else:
-            for i in range((index - largest//2)+1, index + largest//2 +1):
+            for i in range(index - largest//2, math.ceil(index + largest/2)):
                 pal += s[i]
-
+            
         return pal
     
 
-        
 
-print(Solution.longestPalindrome(None, "babad"))
-print(Solution.longestPalindrome(None, "cbbd"))
-print(Solution.longestPalindrome(None, "qwertyuiopoiuytrewq"))
+
+# print(Solution.longestPalindrome(None, "caba")) #    aba
+print(Solution.longestPalindrome(None, "aaaa")) #    aaaa
+print(Solution.longestPalindrome(None, "acc")) #    cc
+print(Solution.longestPalindrome(None, "ccd")) #    cc
+print(Solution.longestPalindrome(None, "cbbd")) #   bb
+print(Solution.longestPalindrome(None, "ccccc")) #  ccccc
+print(Solution.longestPalindrome(None, "ccc")) #    ccc
+print(Solution.longestPalindrome(None, "ac")) #     a
+print(Solution.longestPalindrome(None, "bb"))    #  bb
+print(Solution.longestPalindrome(None, "babad")) #  bad or aba
+print(Solution.longestPalindrome(None, "cbbd")) #   bb
+print(Solution.longestPalindrome(None, "qwertyuiopoiuytrewq"))  #qwery both ways

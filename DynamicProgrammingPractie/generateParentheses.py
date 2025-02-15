a16165ed2c6c7eef933528a36aaa6c5c0c18502a
Ian McKechnie <imckechn@uoguelph.ca>
2025-02-15 16:57:23 -0500

    Optimal approach, though sadly not mine

--- DynamicProgrammingPractie/generateParentheses.py
+++ DynamicProgrammingPractie/generateParentheses.py
@@ -2,59 +2,21 @@ from typing import List
 
 
 class Solution:
-    def subdivide(c): #Once this runs (in brute force) I'll add memoization to this method.
-        c = list(c)
-
-        indexes = [[0, len(c)-1]]
-        depth = 0
-        coordinates = []
-        for i in range(len(c)):
-            if depth == 0 and c[i] == "(":
-                coordinates.append(i)
-                depth +=1
-
-            elif depth != 0 and c[i] == "(":
-                depth +=1
-
-            elif c[i] == ")":
-                depth -= 1
-
-                if depth == 0:
-                    coordinates.append(i)
-
-                    if coordinates not in indexes:
-                        indexes.append(coordinates)
-                    coordinates = []
-
-        answer = []
-        for pair in indexes:
-            new = list(c.copy())
-
-            new.insert(pair[0], "(")
-            new.insert(pair[1] + 1, ")")
-            answer.append(''.join(new))
-
-        return answer
-
-
     def generateParenthesis(self, n: int) -> List[str]:
-        if n == 1:
-            return ["()"]
-        
-        par = Solution.generateParenthesis(self, n-1)
-        copy = par.copy()
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return 
 
-        total = set()
-        for c in copy:
-            total = total.union(set(Solution.subdivide(c)))
+            if left < n:
+                dfs(left + 1, right, s + '(')
 
-        for p in par:
-            if "()" + p not in total:
-                total.add("()" + p)
-            if p + "()" not in total:
-                total.add(p + "()")
+            if right < left:
+                dfs(left, right + 1, s + ')')
 
-        return list(total)
+        res = []
+        dfs(0, 0, '')
+        return res
     
 # print(Solution.generateParenthesis(None, 1))
 

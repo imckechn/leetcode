dde072527bef834fe9fcc327f7c1408d22a4ae64
Ian McKechnie <imckechn@uoguelph.ca>
2025-02-15 15:39:06 -0500

    Getting a duplicates. Going to try and convert to using a set to prevent that

--- DynamicProgrammingPractie/generateParentheses.py
+++ DynamicProgrammingPractie/generateParentheses.py
@@ -2,6 +2,43 @@ from typing import List
 
 
 class Solution:
+    def subdivide(c): #Once this runs (in brute force) I'll add memoization to this method.
+        c = list(c)
+
+        indexes = [[0, len(c)-1]]
+        depth = 0
+        coordinates = []
+        for i in range(len(c)):
+            if depth == 0 and c[i] == "(":
+                coordinates.append(i)
+                depth +=1
+
+            elif depth != 0 and c[i] == "(":
+                depth +=1
+
+            elif c[i] == ")":
+                depth -= 1
+
+                if depth == 0:
+                    coordinates.append(i)
+
+                    if coordinates not in indexes:
+                        indexes.append(coordinates)
+                    coordinates = []
+
+        answer = []
+        for pair in indexes:
+            new = list(c.copy())
+
+            new.insert(pair[0], "(")
+            new.insert(pair[1] + 1, ")")
+            answer.append(''.join(new))
+
+        return answer
+
+
+        
+
     def generateParenthesis(self, n: int) -> List[str]:
         if n == 1:
             return ["()"]
@@ -11,13 +48,18 @@ class Solution:
 
         total = []
         for c in copy:
-            total.append("(" + c + ")")
+            total += Solution.subdivide(c)
 
         for p in par:
-            total.append("()" + p)
+
+            if "()" + p not in total:
+                total.append("()" + p)
 
         return total
     
-print(Solution.generateParenthesis(None, 1))
+# print(Solution.generateParenthesis(None, 1))
 print(Solution.generateParenthesis(None, 3))
 print(Solution.generateParenthesis(None, 5))
+
+# Solution.subdivide("((()))")
+# Solution.subdivide("()()(())(()())")

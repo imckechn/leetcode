18de9b50a9f3732abe00fa7c520baba5713c23c6
Ian McKechnie <imckechn@uoguelph.ca>
2025-02-16 11:37:09 -0500

    Hitting a time limit on a test case. Not ideal. Need to think of a new soln

--- DynamicProgrammingPractie/jumpGame.py
+++ DynamicProgrammingPractie/jumpGame.py
@@ -7,28 +7,24 @@ class Solution:
         jump = [nums[0]]
         currentIndex = [0]
 
-        while currentIndex[-1] + jump[-1] < len(nums):  #Getting an error where my jump is 0
+        while currentIndex[-1] + jump[-1] < len(nums) and currentIndex[-1] != len(nums) -1:  #Getting an error where my jump is 0
             if jump[-1] == 0:
                     jump.pop()
                     currentIndex.pop()
                     path.pop()
-                    jump[-1] -= 1
 
                     if path == []:
                         return False
+            
+                    jump[-1] -= 1
                     continue
 
-            if nums[currentIndex[-1] + jump[-1]] != 0:
-                path.append(nums[currentIndex[-1] + jump[-1]])
-                currentIndex.append(nums[jump[-1]])
-                jump.append(nums[currentIndex[-1] + jump[-1]])
+            path.append(nums[currentIndex[-1] + jump[-1]])
+            currentIndex.append(currentIndex[-1] + jump[-1])
+            jump.append(nums[currentIndex[-1]])
 
-            else:
-                jump[-1] -= 1
-
-                
-        
         return True
                     
-# print(Solution.canJump(None,  [2,3,1,1,4])) #True
-print(Solution.canJump(None,  [3,2,1,0,4])) #True
+print(Solution.canJump(None,  [2,3,1,1,4])) #True
+print(Solution.canJump(None,  [3,2,1,0,4])) #False
+print(Solution.canJump(None,  [2,0,0])) #True

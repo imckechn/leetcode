d7d328fc48eaade9a6c923f1958828d096dd40b6
Ian McKechnie <imckechn@uoguelph.ca>
2025-02-21 20:34:22 -0500

    Attemping a soln. Not working

--- DynamicProgrammingPractie/jumpGame2.py
+++ DynamicProgrammingPractie/jumpGame2.py
@@ -6,7 +6,6 @@ class Solution:
         return Solution.canJump2(self, nums, 0, 0, [0])
 
         maxReach = 0
-        # indexes =[0]
         for i in range(len(nums)):
             if i > maxReach:
                 return False  # Cannot proceed further
@@ -20,19 +19,15 @@ class Solution:
             if i > maxReach:
                 return False  # Cannot proceed further
             
-            newMaxReach = max(maxReach, i + nums[i])
-
-            if newMaxReach != maxReach and i + nums[i] - 1 != i:
-                maxReach = min(Solution.canJump2(self, nums, i, newMaxReach, indexes), Solution.canJump2(self, nums, i, newMaxReach-1, indexes))
-                indexes.append(maxReach)
+            maxReach = min(Solution.canJump2(self, nums, i+1, maxReach, indexes), Solution.canJump2(self, nums, i + nums[i], maxReach, indexes))
 
             if maxReach >= len(nums) - 1:
                 return len(indexes)  # Reached or surpassed last index
         return len(nums) + 1
                     
 # print(Solution.canJump(None,  [1,1,1,1,1,1,1,1,1])) #True
-print(Solution.canJump(None,  [2,3,1,1,4])) #True
-print(Solution.canJump(None,  [3,2,1,0,4])) #False
+# print(Solution.canJump(None,  [2,3,1,1,4])) #True
+print(Solution.canJump(None,  [3,2,2,0,4])) #False
 print(Solution.canJump(None,  [2,0,0])) #True
 print(Solution.canJump(None,  [2,3,1,1,4])) #2
 print(Solution.canJump(None,  [2,3,0,1,4])) #2

6f714d4f8bb855c17a213789ac96a12f033006e1
Ian McKechnie <imckechn@uoguelph.ca>
2025-02-23 10:37:35 -0500

    Finally got it. Had my if statement in the wrong place

--- DynamicProgrammingPractie/jumpGame2.py
+++ DynamicProgrammingPractie/jumpGame2.py
@@ -3,35 +3,32 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool: #Should be able to take a greedy approach to jumping
-        if len(nums) == 1 or len(nums) == 1:
+        if len(nums) == 1 or len(nums) == 0:
+            return 0
+        elif nums[0] >= len(nums) -1:
             return 1
 
         far = nums[0]
-        jumpCount = 1
+        jumpCount = 1       
         nextFarthest = nums[0] + nums[far]
 
-        if far == len(nums) - 1:
-            return jumpCount
-
         for i in range(1, len(nums)):
-            if i == far:
-                far = nextFarthest
-                nextFarthest = nums[i] + i + 1
-                jumpCount += 1
-
             if nums[i] + i >= nextFarthest:
                 nextFarthest = nums[i] + i
 
-            if nextFarthest == len(nums) - 1:
-                return jumpCount +1
+            if nextFarthest >= len(nums) - 1:
+                return jumpCount+1
+
+            if i == far:
+                jumpCount += 1
+                far = nextFarthest
+                nextFarthest = nums[i] + i + 1
+                
+                if far >= len(nums) - 1:
+                    return jumpCount
 
         return jumpCount
 
                     
-print(Solution.canJump(None,  [2,1])) #8
-print(Solution.canJump(None,  [1,1,1, 1,1,1, 1,1,1])) #8
-print(Solution.canJump(None,  [2,3,1,1,4])) #2
-print(Solution.canJump(None,  [3,2,2,0,4])) #2
-print(Solution.canJump(None,  [2,0,0])) #1
-print(Solution.canJump(None,  [2,3,0,1,4])) #2
-
+# print(Solution.canJump(None,  [0])) #8
+print(Solution.canJump(None,  [1,2,3])) #8

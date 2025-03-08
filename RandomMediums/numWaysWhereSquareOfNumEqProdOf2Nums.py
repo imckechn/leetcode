ce8d6c66a45271c790b9fa1cec49d95eff31758f
Ian McKechnie <imckechn@uoguelph.ca>
2025-03-07 20:33:18 -0500

    Better soln. It's just not working yet

--- RandomMediums/numWaysWhereSquareOfNumEqProdOf2Nums.py
+++ RandomMediums/numWaysWhereSquareOfNumEqProdOf2Nums.py
@@ -1,28 +1,36 @@
 from typing import List
 
 class Solution:
-    def findTriplet(val, nums):
+    def findTriplet(factors, nums):
         triplets = 0
-        val *= val
 
-        for i in range(len(nums)):
-            kill = False
-            for j in range(len(nums)):
-                if i == j:
-                    continue
-
-                if val == nums[i] * nums[j]:
-                    triplets += 1
+        for pair in factors:
+            a = 0
+            b = 0
+            for i in range(len(nums)):
+                if nums[i] == pair[0]:
+                    a += 1
                 
-                elif val < nums[i] * nums[j]:
-                    if j == 0:
-                        kill = True
-                    continue
-            
-            if kill:
-                break
+                elif nums[i] == pair[1]:
+                    b += 1
+
+            if pair[0] == pair[1] and triplets > 1: #Issue is here
+                triplets += a
+            else:
+                triplets += a * b
 
         return triplets
+
+    
+    def findFactors(number):
+        factors = [[1, number]]
+        for i in range(2, number//2):
+            factor = int(number/i)
+            if number == i * factor:
+                factors.append([i, factor])
+
+        return factors
+
 
     def numTriplets(self, nums1: List[int], nums2: List[int]) -> int:
         triplets = 0
@@ -30,13 +38,17 @@ class Solution:
         nums2.sort()
 
         for num in nums1:
-            triplets += Solution.findTriplet(num, nums2)
+            factors = Solution.findFactors(num * num)
+            triplets += Solution.findTriplet(factors, nums2)
 
         for num in nums2:
-            triplets += Solution.findTriplet(num, nums1)
+            factors = Solution.findFactors(num * num)
+            triplets += Solution.findTriplet(factors, nums2)
 
-        return int(triplets/2)
 
-print(Solution.numTriplets(None, [7,4], [5,2,8,9]))
-print(Solution.numTriplets(None, [1,1], [1,1,1]))
-print(Solution.numTriplets(None, [7,7,8,3], [1,2,9,7]))
+        return triplets
+
+print(Solution.numTriplets(None, [7,4], [5,2,8,9])) # 1
+print(Solution.numTriplets(None, [1,1], [1,1,1])) #9
+print(Solution.numTriplets(None, [7,7,8,3], [1,2,9,7])) #2
+ 

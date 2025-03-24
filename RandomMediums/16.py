99b1c9071a53a24f3911e7c8f1df47700064c9af
Ian McKechnie <imckechn@uoguelph.ca>
2025-03-23 20:22:58 -0400

    Trying the double pointer approach. Getting tired though. Will need to pick this back up tomorrow hopefully.

--- RandomMediums/16.py
+++ RandomMediums/16.py
@@ -9,56 +9,33 @@ class Solution:
         return a + b
     
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        best = abs(target) - abs((nums[0] + nums[1] + nums[2]))
+        best = abs(target) - abs(Solution.distance(target, nums[0] + nums[1] + nums[2]))
         values = [nums[0], nums[1], nums[2]]
-
-        positives = []
-        negatives = []
-        zeros = 0
-
-        for num in nums:
-            if num < 0:
-                negatives.append(num)
-            elif num > 0:
-                positives.append(num)
-            else:
-                zeros += 1
-
-        if zeros > 1:
-            for pos in positives:
-                for neg in negatives:
-                    if target == pos + neg:
-                        return target
-
-                    if abs(Solution.distance(target, pos + neg)) < best:
-                        best = abs(pos + neg)
-                        values = [pos, neg, 0]
-
         nums.sort()
+
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
+        
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                diff = Solution.distance(target, nums[i] + nums[j] + nums[k])
+                if  abs(target) - abs(diff) < best:
+                    values = [nums[i], nums[j], nums[k]]
+                    best =  abs(target) - abs(diff)
 
-            left = i + 1
-            right = len(nums) - 1
-
-            while left < right:
-                if nums[i] + nums[left] + nums[right] == target:
-                    return target
-                
-                elif abs(Solution.distance(target, (nums[i] + nums[left] + nums[right]))) < best:
-                    best = abs(nums[i] + nums[left] + nums[right])
-                    values = [nums[i], nums[left], nums[right]]
-                    left += 1
-
+                    if best == 0:
+                        return sum([x] for x in values)
+                    
                 else:
-                    if nums[i] + nums[left] + nums[right] < target:
-                        left += 1
-                    else:
-                        right -= 1
+                    if diff > 0:
+                        k -= 1
+                    else: 
+                        j += 1
+        
+        return sum(x for x in values)
 
-        return sum(val for val in values)
 
-
-# print(Solution.threeSumClosest(None, [-1,2,1,-4], 1)) #2
+print(Solution.threeSumClosest(None, [-1,2,1,-4], 1)) #2
 print(Solution.threeSumClosest(None, [1,1,1,0], -100)) #2

6608c781a12f100cf699ce6ffa903c3a8377768f
Ian McKechnie <imckechn@uoguelph.ca>
2025-03-29 12:50:43 -0400

    On the right path but forgot BST logic so need to fix with the new understanding.

--- RandomMediums/99.py
+++ RandomMediums/99.py
@@ -8,43 +8,49 @@ from typing import Optional
 
 
 class Solution:
-    def checkBadValues(root):
-        address = []
+    # def checkBadValues(root):
+    #     address = []
         
-        if root == None or (root.left == None and root.right == None):
-            return address
+    #     if root == None or (root.left == None and root.right == None):
+    #         return address
 
-        #check no left
-        if root.left == None:
-            if root.val > root.right.val:
-                address.append(root)
-                return address + Solution.checkBadValues(root.right)
+    #     #check no left
+    #     if root.left == None:
+    #         if root.val > root.right.val:
+    #             address.append(root)
+    #             return address + Solution.checkBadValues(root.right)
 
-        #check no right
-        if root.right == None:
-            if root.val > root.left.val:
-                address.append(root)
-                return address + Solution.checkBadValues(root.left)
+    #     #check no right
+    #     if root.right == None:
+    #         if root.val > root.left.val:
+    #             address.append(root)
+    #             return address + Solution.checkBadValues(root.left)
 
-        #Need to check if it's a leaf node
-        if root.val > root.left.val or root.val > root.right.val:
-            address.append(root)
+    #     #Need to check if it's a leaf node
+    #     if root.val > root.left.val or root.val > root.right.val:
+    #         address.append(root)
 
-        return address + Solution.checkBadValues(root.left) + Solution.checkBadValues(root.right)
+    #     return address + Solution.checkBadValues(root.left) + Solution.checkBadValues(root.right)
 
+    #Need to run depth first search, then on your way back up, swap the values
     def recoverTree(self, root: Optional[TreeNode]) -> None:
-        values = Solution.checkBadValues(root)
-
-        if len(values) > 2:
-            print("Too many bad values")
+        if root == None:
             return
-        else:
-            values[0].val, values[1].val = values[1].val, values[0].val
+        
+        Solution.recoverTree(None, root.left)
+        Solution.recoverTree(None, root.right)
 
-a = TreeNode(3)
-b = TreeNode(2)
-c = TreeNode(1, a, b)
+        if root.left != None and root.val > root.left.val:
+            root.val, root.left.val = root.left.val, root.val
+
+        if root.right != None and root.val > root.right.val:
+            root.val, root.right.val = root.right.val, root.val
+
+        return
 
 
+a = TreeNode(2)
+b = TreeNode(1)
+c = TreeNode(3, a, b)
 
-print(Solution.recoverTree(None, c))
+Solution.recoverTree(None, c)

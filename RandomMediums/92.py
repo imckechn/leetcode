df196ad747adb6a4e13723582b40562e816377f5
Ian McKechnie <imckechn@uoguelph.ca>
2025-10-10 13:01:35 -0400

    Done

--- RandomMediums/92.py
+++ RandomMediums/92.py
@@ -9,28 +9,11 @@ class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         copy = head
 
-        while copy.val != left:
+        while copy.next != None and copy.next.val != left:
             copy = copy.next
 
-        first = copy.next
-        if first.next == None:
-            first.next = copy
-            copy.next = None
-
-            return first
-        
-        second = first.next
-
-        copy.next, first.next = first.next, copy
-        copy = first
-        first = copy.next
-
-        while second != None and copy.val != right:
-            first.next = second.next
-            second.next = copy
-
-            copy = second
-            second = first.next
+        if copy.next != None:
+            copy.next = Solution.reverseLinkedList(copy.next, right)
 
         return head
 
@@ -63,11 +46,17 @@ class Solution:
 
 
 
-a = ListNode(6)
-b = ListNode(5, a)
-c = ListNode(4, b)
-d = ListNode(3, c)
-f = ListNode(2, d)
-g = ListNode(1, f)
+a = ListNode(5)
+b = ListNode(3, a)
 
-Solution.reverseBetween(None, g, 2, 5)
+Solution.reverseBetween(None, b, 1, 2)
+
+
+# a = ListNode(6)
+# b = ListNode(5, a)
+# c = ListNode(4, b)
+# d = ListNode(3, c)
+# f = ListNode(2, d)
+# g = ListNode(1, f)
+
+# Solution.reverseBetween(None, g, 2, 5)

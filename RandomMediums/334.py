3291ea480dc46ce627533888ab7ba3d434322d52
Ian McKechnie <imckechn@uoguelph.ca>
2025-10-16 14:15:49 -0400

    Solved in O(n) time

--- RandomMediums/334.py
+++ RandomMediums/334.py
@@ -18,75 +18,72 @@ class Solution:
         s = nums[0]
         m = None
         for i in range(1, len(nums)):
-            if nums[i] < s and i < len(nums)-2:
+            if nums[i] <= s:
                 s = nums[i]
-                m = None
+                continue
+
+            elif m == None or nums[i] <= m:
+                m = nums[i]
+                continue
 
             else:
-                if m == None:
-                    m = nums[i]
-                else:
-                    if nums[i] <= m:
-                        m = nums[i]
-                    elif s < m and m < nums[i]:
-                        return True
-            
+                return True
         return False
 
 
-# #Test 1
-# input = [1,2,3,4,5]
-# expected = True
-# answer = Solution.increasingTriplet(None, input)
+#Test 1
+input = [1,2,3,4,5]
+expected = True
+answer = Solution.increasingTriplet(None, input)
 
-# if (answer == expected):
-#     print("Q1: Passed")
-# else:
-#     print("Q1: Failed, should be " + str(expected))
+if (answer == expected):
+    print("Q1: Passed")
+else:
+    print("Q1: Failed, should be " + str(expected))
     
 
-# #Test 2
-# input = [5,4,3,2,1]
-# expected = False
-# answer = Solution.increasingTriplet(None, input)
+#Test 2
+input = [5,4,3,2,1]
+expected = False
+answer = Solution.increasingTriplet(None, input)
 
-# if (answer == expected):
-#     print("Q2: Passed")
+if (answer == expected):
+    print("Q2: Passed")
 # else:
 #     print("Q2: Failed, should be " + str(expected))
 
 
-# #Test 3
-# input = [2,1,5,0,4,6]
-# expected = True
-# answer = Solution.increasingTriplet(None, input)
+#Test 3
+input = [2,1,5,0,4,6]
+expected = True
+answer = Solution.increasingTriplet(None, input)
 
-# if (answer == expected):
-#     print("Q3: Passed")
-# else:
-#     print("Q3: Failed, should be " + str(expected))
-
-
-# #Test 4
-# input = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-# expected = False
-# answer = Solution.increasingTriplet(None, input)
-
-# if (answer == expected):
-#     print("Q4: Passed")
-# else:
-#     print("Q4: Failed, should be " + str(expected))
+if (answer == expected):
+    print("Q3: Passed")
+else:
+    print("Q3: Failed, should be " + str(expected))
 
 
-# #Test 5
-# input = [5,1,6]
-# expected = False
-# answer = Solution.increasingTriplet(None, input)
+#Test 4
+input = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+expected = False
+answer = Solution.increasingTriplet(None, input)
 
-# if (answer == expected):
-#     print("Q5: Passed")
-# else:
-#     print("Q5: Failed, should be " + str(expected))
+if (answer == expected):
+    print("Q4: Passed")
+else:
+    print("Q4: Failed, should be " + str(expected))
+
+
+#Test 5
+input = [5,1,6]
+expected = False
+answer = Solution.increasingTriplet(None, input)
+
+if (answer == expected):
+    print("Q5: Passed")
+else:
+    print("Q5: Failed, should be " + str(expected))
 
 
 #Test 6
@@ -98,3 +95,14 @@ if (answer == expected):
     print("Q6: Passed")
 else:
     print("Q6: Failed, should be " + str(expected))
+
+
+#Test 7
+input = [9,10,5,11,10,9,8]
+expected = True
+answer = Solution.increasingTriplet(None, input)
+
+if (answer == expected):
+    print("Q7: Passed")
+else:
+    print("Q7: Failed, should be " + str(expected))

48dbba914095caf6ab9550bcc72667314c626d05
Ian McKechnie <imckechn@uoguelph.ca>
2025-10-22 21:56:00 -0400

    Works for most cases. Can't see why it's breaking cases with >1k nums.

--- RandomMediums/215.py
+++ RandomMediums/215.py
@@ -1,55 +1,60 @@
 from typing import List
 
-
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = Solution.heapify(nums)[k-1]
-
-        return Solution.popOffHeap(k)[0]
-    
-
-    def popOffHeap(k):
-        
-    
-
-
-    #Create the max heap
-    def heapify(nums):
         heap = []
-
         for num in nums:
-            heap.append(num)
+            heap.insert(0, num)
+            Solution.heapify_down(heap)
 
-            index = len(heap) - 1
-            while heap[index] > heap[int(index/2)]:
-                heap[index], heap[int(index/2)] = heap[int(index/2)], heap[index]
-                index = int(index/2)
+        for i in range(k-1):
+            heap[0] = heap.pop()
+            Solution.heapify_down(heap)
 
-        return heap
+        return heap[0]
 
+    def heapify_down(heap, index=0):
+        size = len(heap)
+        smallest = index
+
+        left = 2 * index + 1
+        right = 2 * index + 2
+
+        # Check if right child is larger
+        if right < size and heap[right] > heap[smallest]:
+            smallest = right
+
+        # Check if left child is larger
+        if left < size and heap[left] > heap[smallest]:
+            smallest = left
+
+        # If a child is smaller, swap and continue recursively
+        if smallest != index:
+            heap[index], heap[smallest] = heap[smallest], heap[index]
+            Solution.heapify_down(heap, smallest)
 
 sol = Solution()
 
-# #Test 1
-# input = [3,2,1,5,6,4]
-# k = 2
-# expected = 5
-# ans = sol.findKthLargest(input, k)
-# if ans == expected:
-#     print("Test 1 passed")
-# else:
-#     print("Test 1 failed")
+#Test 1
+input = [3,2,1,5,6,4]
+k = 2
+expected = 5
+ans = sol.findKthLargest(input, k)
+if ans == expected:
+    print("Test 1 passed")
+else:
+    print("Test 1 failed")
 
 
-# #Test 2
-# input = [3,2,3,1,2,4,5,5,6]
-# k = 4
-# expected = 4
-# ans = sol.findKthLargest(input, k)
-# if ans == expected:
-#     print("Test 2 passed")
-# else:
-#     print("Test 2 failed")
+#Test 2
+input = [3,2,3,1,2,4,5,5,-6]
+k = 4
+expected = 3
+ans = sol.findKthLargest(input, k)
+if ans == expected:
+    print("Test 2 passed")
+else:
+    print("Test 2 failed")
 
 
 #Test 3

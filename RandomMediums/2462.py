7793268d61bf87102f634ef8937625ad2dcfd26a
Ian McKechnie <imckechn@uoguelph.ca>
2025-10-28 12:52:52 -0400

    Better soln but still times out

--- RandomMediums/2462.py
+++ RandomMediums/2462.py
@@ -7,50 +7,86 @@ class Solution:
         second = True
 
         for i in range(k):
-            if candidates*2 < len(costs):
+            if candidates*2 < len(costs):                
                 can1 = costs[:candidates]
                 mid = costs[candidates:len(costs)-candidates]
                 can2 = costs[len(costs)-candidates:]
 
-                can1.sort()
-                can2.sort()
+                if first:
+                    can1.sort()
+                    can2.sort()
+                    first = False
 
                 if can1[0] <= can2[0]:
                     cost += can1.pop(0)
+                    new = mid.pop(0)
+                    for i in range(len(can1)):
+                        if can1[i] > new:
+                            can1.insert(i, new)
+                            new = -1
+                            break
+                    
+                    if new != -1:
+                        can1.append(new)
+                    
                 else:
                     cost += can2.pop(0)
+                    new = mid.pop()
+                    for i in range(len(can2)):
+                        if can2[i] > new:
+                            can2.insert(i, new)
+                            new = -1
+                            break
+
+                    if new != -1:
+                        can2.append(new)
 
                 costs = can1 + mid + can2
 
             else:
-                costs.sort()
+                if second:
+                    costs.sort()
+                    second = False
                 cost += costs.pop(0)
 
         return cost
 
 sol = Solution()
 
-#Test 1
-costs = [17,12,10,2,7,2,11,20,8]
-k = 3
-can = 4
-expected = 11
+# #Test 1
+# costs = [17,12,10,2,7,2,11,20,8]
+# k = 3
+# can = 4
+# expected = 11
+
+# ans = sol.totalCost(costs, k, can)
+# if ans == expected:
+#     print("Test 1 passed")
+# else:
+#     print("Test 2 failed")
+
+
+# #Test 2
+# costs = [1,2,4,1]
+# k = 3
+# can = 3
+# expected = 4
+
+# ans = sol.totalCost(costs, k, can)
+# if ans == expected:
+#     print("Test 2 passed")
+# else:
+#     print("Test 2 failed")
+
+
+#Test 3
+costs = [31,25,72,79,74,65,84,91,18,59,27,9,81,33,17,58]
+k = 11
+can = 2
+expected = 423
 
 ans = sol.totalCost(costs, k, can)
 if ans == expected:
-    print("Test 1 passed")
+    print("Test 3 passed")
 else:
-    print("Test 2 failed")
-
-
-#Test 2
-costs = [1,2,4,1]
-k = 3
-can = 3
-expected = 4
-
-ans = sol.totalCost(costs, k, can)
-if ans == expected:
-    print("Test 2 passed")
-else:
-    print("Test 2 failed")
+    print("Test 3 failed")

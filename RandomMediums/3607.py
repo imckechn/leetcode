785026eba226ffa22222166ecb8159e521ef6e18
Ian McKechnie <imckechn@uoguelph.ca>
2025-11-06 13:54:05 -0500

    Another method to solve. Broken tho

--- RandomMediums/3607.py
+++ RandomMediums/3607.py
@@ -5,7 +5,6 @@ from typing import List
 class Solution:
     def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
         map = {x+1:[] for x in range(c)}
-        smallestFound = {x+1:x+1 for x in range(c)}
         disabled = []
         answer = []
 
@@ -14,7 +13,41 @@ class Solution:
             map[connection[0]].append(connection[1])
             map[connection[1]].append(connection[0])
 
-        #Perform the queries
+        #Create the groups
+        groups = []
+        for node in map:
+            groupExists = False
+            for i in range(len(groups)):
+            # for group in groups:
+                if node in groups[i]:
+                    groupExists = True
+                    temp = set(map[node])
+                    temp.add(node)
+                    groups[i] = groups[i].union(temp)
+                    break
+
+                else:
+                    for child in map[node]:
+                        if child in groups[i]:
+                            groupExists = True
+                            temp = set(map[node])
+                            temp.add(node)
+                            groups[i] = groups[i].union(temp)
+                            break
+                
+            if not groupExists:
+                temp = set(map[node])
+                temp.add(node)
+                groups.append(temp)
+
+        #Turn the groups into lists and sort by ascending
+        sortedGroups = []
+        for group in groups:
+            g = list(group)
+            g.sort()
+            sortedGroups.append(g)
+
+        # #Perform the queries
         for query in queries:
             
             #Disabled
@@ -29,34 +62,18 @@ class Solution:
             else:
                 current = query[1]
 
-                #See if we've already found the smallest
-                if current <= c and smallestFound[current] not in disabled:
-                    answer.append(smallestFound[current])
-                    continue
-
-                queue = []
-                visited = []
-                smallest = inf
-
-                while True:
-                    if current not in disabled and current < smallest:
-                        smallest = current
-
-                    for elem in map[current]:
-                        if elem not in visited:
-                            queue.append(elem)
-                    
-                    visited.append(current)
-
-                    if len(queue) == 0:
+                notFound = True
+                for group in sortedGroups:
+                    if current in group:
+                        for elem in group:
+                            if elem not in disabled:
+                                notFound = False
+                                answer.append(elem)
+                                break
                         break
-                    current = queue.pop(0)
 
-                if smallest == inf:
+                if notFound:
                     answer.append(-1)
-                else:
-                    smallestFound[query[1]] = smallest
-                    answer.append(smallest)
         return answer
 
 
@@ -64,27 +81,27 @@ class Solution:
 
 sol = Solution()
 
-# #Test 2
-# con = []
-# queries = [[1,1],[2,1],[1,1]]
-# expected = [1, -1]
+#Test 1
+con = [[1,2],[2,3],[3,4],[4,5]]
+queries = [[1,3],[2,1],[1,1],[2,2],[1,2]]
+expected = [3,2,3]
 
-# ans = sol.processQueries(3, con, queries)
-# if ans != expected:
-#     print("Test 2 failed")
-# else:
-#     print("Test 2 passed")
+ans = sol.processQueries(5, con, queries)
+if ans != expected:
+    print("Test 1 failed")
+else:
+    print("Test 1 passed")
 
-# #Test 1
-# con = [[1,2],[2,3],[3,4],[4,5]]
-# queries = [[1,3],[2,1],[1,1],[2,2],[1,2]]
-# expected = [3,2,3]
+#Test 2
+con = []
+queries = [[1,1],[2,1],[1,1]]
+expected = [1, -1]
 
-# ans = sol.processQueries(5, con, queries)
-# if ans != expected:
-#     print("Test 1 failed")
-# else:
-#     print("Test 1 passed")
+ans = sol.processQueries(3, con, queries)
+if ans != expected:
+    print("Test 2 failed")
+else:
+    print("Test 2 passed")
 
 #Test 3
 con = []
@@ -93,6 +110,6 @@ expected = [3,2,3]
 
 ans = sol.processQueries(1, con, queries)
 if ans != expected:
-    print("Test 3 failed")
+    print("Test 3 failed, got " + str(ans))
 else:
     print("Test 3 passed")

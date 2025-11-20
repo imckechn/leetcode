2dce628c399864edda0301450091b680f768a738
Ian McKechnie <imckechn@uoguelph.ca>
2025-11-20 11:10:56 -0500

    Don't really understand the question logic for this one.

---
+++ RandomMediums/1475.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        for i in range(0, len(prices)-1):
+            j = i+1
+
+            while j < len(prices):
+                if prices[i] >= prices[j]:
+                    prices[i] -= prices[j]
+                    break
+                else:
+                    j += 1
+
+        return prices

aa910905df3be71726e6786773f48e2600b49e66
Ian McKechnie <imckechn@uoguelph.ca>
2025-11-20 14:01:41 -0500

    Made some performance tweeks but it's still slow as balls. Might have to look at the soln for this one.

--- RandomMediums/815.py
+++ RandomMediums/815.py
@@ -3,11 +3,8 @@ from typing import List
 
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-        visitedStops = []
-        visitedRoutes = []
-
         connections = self.createConnectionsMap(routes)
-        return self.dfs(connections, visitedStops, visitedRoutes, source, target)
+        return self.dfs(connections, [], [], source, target)
 
     def createConnectionsMap(self, routes):
         ans = {}
@@ -20,23 +17,25 @@ class Solution:
                     ans[stop] = [route]
         return ans
     
-    def dfs(self, connections, visitedStops, visitedRoutes, source, target):
-        if target == source:
-            return len(visitedRoutes) + 1
-        elif source in visitedStops:
-            return -1
-        
-        visitedStops.append(source)       
+    def dfs(self, connections, visitedStops, visitedRoutes, source, target):        
+        visitedStops.append(source)
 
         smallest = inf
         for route in connections[source]:
             if route in visitedRoutes:
                 continue
 
+            elif target in route:
+                return len(visitedRoutes) + 1
+
             for stop in route:
-                ans = self.dfs(connections, visitedStops.copy(), visitedRoutes.copy(), stop, target)
-                if ans != -1 and ans < smallest:
-                    smallest = ans
+                if stop in visitedStops or stop == source:
+                    continue
+
+                if len(connections[stop]) > 1:
+                    ans = self.dfs(connections, visitedStops.copy(), visitedRoutes.copy(), stop, target)
+                    if ans != -1 and ans < smallest:
+                        smallest = ans
 
             visitedRoutes.append(route)
 
@@ -63,16 +62,16 @@ target = 12
 ans = sol.numBusesToDestination(routes, source, target)
 print(ans)
 
+routes = [[25,33],[3,5,13,22,23,29,37,45,49],[15,16,41,47],[5,11,17,23,33],[10,11,12,29,30,39,45],[2,5,23,24,33],[1,2,9,19,20,21,23,32,34,44],[7,18,23,24],[1,2,7,27,36,44],[7,14,33]]
+source = 7
+target = 42
 
-# for i in range(len(route)):
-                # if i == len(route)-1:
-                #     if route[i] in ans:
-                #         ans[route[i]].append(route[0])
-                #     else:
-                #         ans[route[i]] = [route[0]]
+ans = sol.numBusesToDestination(routes, source, target)
+print(ans)
 
-                # else:
-                #     if route[i] in ans:
-                #         ans[route[i]].append(route[i+1])
-                #     else:
-                #         ans[route[i]] = [route[i+1]]
+routes = [[1,9,12,20,23,24,35,38],[10,21,24,31,32,34,37,38,43],[10,19,28,37],[8],[14,19],[11,17,23,31,41,43,44],[21,26,29,33],[5,11,33,41],[4,5,8,9,24,44]]
+source = 37
+target = 28
+
+ans = sol.numBusesToDestination(routes, source, target)
+print(ans)

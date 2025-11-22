b7b6cc437b3b62ba1026c312a6eb10e1d5b501f2
Ian McKechnie <imckechn@uoguelph.ca>
2025-11-22 16:29:44 -0500

    The correct soln. Nobody's coming up with this in an interview. That's cooked.

--- 792.py
+++ 792.py
@@ -1,22 +1,27 @@
 from typing import List
 
-class Solution:
-    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        wordCount = len(words)
-        for letter in s:
-            if words == []:
-                break
+class Solution(object):
+    def numMatchingSubseq(self, S, words):
+        ans = 0
+        heads = [[] for _ in range(26)]
+        for word in words:
+            it = iter(word)
+            heads[ord(next(it)) - ord('a')].append(it)
 
-            i = 0
-            while i < len(words) and words != []:
-                if words[i][0] == letter:
-                    words[i] = words[i][1:]
+        for letter in S:
+            letter_index = ord(letter) - ord('a')
+            old_bucket = heads[letter_index]
+            heads[letter_index] = []
 
-                    if words[i] == "":
-                        words.pop(i)
-                        i -= 1
-                i += 1
-        return wordCount - len(words)
+            while old_bucket:
+                it = old_bucket.pop()
+                nxt = next(it, None)
+                if nxt:
+                    heads[ord(nxt) - ord('a')].append(it)
+                else:
+                    ans += 1
+
+        return ans
             
 sol = Solution()
 

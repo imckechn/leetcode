adfd82791b910976db23b0c63769dc9f0f88a731
Ian McKechnie <imckechn@uoguelph.ca>
2025-11-23 18:45:26 -0500

    Possible soln but overly complex. Need to start thinking of a soln before jumping into it.

---
+++ JavaLeetcodeProblems/src/nine.java
@@ -0,0 +1,32 @@
+public class nine {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        char[] number = String.valueOf(x).toCharArray();
+        if (number.length == 1) {
+            return true;
+        }
+
+        int left = number.length/2;
+        int right = 0;
+        if (number.length%2 == 0) {
+            right = left;
+            left -= 1;
+        } else {
+            right = left + 1;
+            left -= 1;
+        }
+
+        while (left >= 0) {
+            if (number[left] != number[right]) {
+                return false;
+            }
+            left -= 1;
+            right += 1;
+        }
+
+        return true;
+    }
+}

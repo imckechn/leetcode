8ef3af22b21bf1e166ac4e901c5222a30c761bf2
Ian McKechnie <imckechn@uoguelph.ca>
2025-11-24 15:28:18 -0500

    getting better at this.

---
+++ JavaLeetcodeProblems/src/twentyOneSavage.java
@@ -0,0 +1,34 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class twentyOneSavage {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode current = head;
+
+        while (list1 != null || list2 != null) {
+            if (list1 == null) {
+                current.next = list2;
+                break;
+            } else if (list2 == null) {
+                current.next = list1;
+                break;
+            }
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        return head.next;
+    }
+}

0538084a1443a58b4f8b5bbc6d984df26036b1c5
Ian McKechnie <imckechn@uoguelph.ca>
2025-11-26 17:21:49 -0500

    Not getting this, you need to use dynamic programming and I'm not learning that, 2D dyanmic programming makes no sense what so ever

--- 91.py
+++ 91.py
@@ -3,26 +3,38 @@ class Solution:
     MAX = 26
 
     def numDecodings(self, s: str) -> int:
-        if len(s) == 0:
-            return 1
-        elif len(s) == 1 and s[0] != "0":
-            return 1
-        elif s[0] == "0":
-            return 0
+        total = 1
+        length = len(s)
+        pointer = 0
 
-        if int(s[0]) >= self.MIN and int(s[:2]) <= self.MAX:
-            return self.numDecodings(s[1:]) + self.numDecodings(s[2:])
-        
-        elif int(s[0]) >= self.MIN:
-            return self.numDecodings(s[1:])
-        
-        elif int(s[:2]) <= self.MAX:
-            return self.numDecodings(s[2:])
-
-        else:
+        if s[0] == "0":
             return 0
+        elif length == 1:
+            return 1
+
+        while pointer < length: 
+            if pointer+1 == length and s[pointer] != "0":
+                total += 1
+                pointer += 1
+            elif int(s[pointer]) >= self.MIN and int(s[:pointer+2]) < self.MAX and s[:pointer+1] != "0" and pointer+2 < length and s[pointer+1] != "0":
+                total *= 2
+                pointer += 2
+            elif int(s[pointer]) >= self.MIN and int(s[:pointer+2]) < self.MAX and pointer == length-2  and s[pointer+1] != "0":
+                total *= 2
+                pointer += 2
+            elif pointer+1 < length and s[pointer] == "0" and s[pointer+1] == "0":
+                return 0
+            elif pointer+2 < length and s[pointer] != "0" and s[pointer+1] != "0" and s[pointer+2] != "0" and pointer+3 == length:
+                break
+            else:
+                pointer += 1
+        return total
+            
+
         
 
 sol = Solution()
+print(sol.numDecodings("10"))
+print(sol.numDecodings("226"))
 print(sol.numDecodings("12"))
 print(sol.numDecodings("11106"))

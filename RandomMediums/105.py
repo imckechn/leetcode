eb5e4e9f659b1c819b819ae41d7044780e9e55e1
Ian McKechnie <imckechn@uoguelph.ca>
2025-11-28 14:34:02 -0500

    Couldn't figure out what to do. Checked the soln, impressive, would not have guessed this on my own

--- RandomMediums/105.py
+++ RandomMediums/105.py
@@ -8,27 +8,59 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    def createTree(self, preorder, inorder, indexes, change):
         if preorder == [] or inorder == []:
             return None
 
         head = TreeNode(preorder[0])
 
-        leftInorder = inorder[:inorder.index(preorder[0])]
-        rightInorder = inorder[inorder.index(preorder[0])+1:]
+        leftInorder = inorder[:change + indexes[preorder[0]]]
+        rightInorder = inorder[change + indexes[preorder[0]]+1:]
+        leftInorderHash = {}
+
+        for left in leftInorder:
+            leftInorderHash[left] = True
 
         leftPreorder = []
         rightPreorder = []
 
         for num in preorder:
-            if num in leftInorder:
+            if num in leftInorderHash:
                 leftPreorder.append(num)
             elif num != head.val:
                 rightPreorder.append(num)
 
-        head.left = self.buildTree(leftPreorder, leftInorder)
-        head.right = self.buildTree(rightPreorder, rightInorder)
+        head.left = self.createTree(leftPreorder, leftInorder, indexes, indexes[preorder[0]]+1)
+        head.right = self.createTree(rightPreorder, rightInorder, indexes, indexes[preorder[0]])
         return head
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        indexes = {}
+        for i, num in enumerate(inorder):
+            indexes[num] = i
+
+        return self.createTree(preorder, inorder, indexes, 0)
+    
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        idx_map = {val: i for i, val in enumerate(inorder)}
+        self.index = 0
+
+        def helper(start, end):
+            if start > end:
+                return None
+            root_val = preorder[self.index]
+            self.index += 1
+            root = TreeNode(root_val)
+
+            mid = idx_map[root_val]
+            root.left = helper(start, mid - 1)
+            root.right = helper(mid + 1, end)
+            return root
+
+        return helper(0, len(inorder) - 1)
+
     
 sol = Solution()
 preorder = [3,9,20,15,7]

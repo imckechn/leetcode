1f97c7427b9251f226f1f1d3817bcf800a419d96
Ian McKechnie <imckechn@uoguelph.ca>
2025-12-16 13:44:13 -0500

    Sheesh this one is still not intuitive

---
+++ 75 Questions/238.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        leftToRight = [1]
+        ans = []
+        for i in range(len(nums)):
+            leftToRight.append(leftToRight[i] * nums[i])
+
+        nums.append(1)
+        sum = 1
+        answer = []
+        for i in range(len(nums)-2, -1, -1):
+            answer.insert(0, leftToRight[i] * sum)
+            sum *= nums[i]
+
+        return answer
+
+sol= Solution()
+input = [1,2,3,4]
+expected = [24,12,8,6]
+
+sol.productExceptSelf(input)

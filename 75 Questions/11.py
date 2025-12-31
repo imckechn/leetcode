ec5d132d2e5d2a21e4043815944ed1a6517571fa
Ian McKechnie <imckechn@uoguelph.ca>
2025-12-30 23:03:20 -0500

    Solved in O(N). Probably the last of the year. Thanks for everything 2025, lots of ups and downs but glad it happened none the less. -Ian

--- 75 Questions/11.py
+++ 75 Questions/11.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         right = len(height)-1
@@ -5,10 +7,14 @@ class Solution:
         maxVal = 0
 
         while left < right:
-            maxVal = max(maxVal, min(height[left], height[right] * (right - left)))
+            maxVal = max(maxVal, min(height[left], height[right]) * (right - left))
 
             if height[left] < height[right]:
                 left += 1
             else:
                 right -=1
         return maxVal
+    
+
+sol = Solution()
+sol.maxArea([1,8,6,2,5,4,8,3,7])

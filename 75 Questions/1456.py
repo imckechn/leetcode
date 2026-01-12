d72297892d5385af90d33b2e3e429f0a011acb99
Ian McKechnie <imckechn@uoguelph.ca>
2026-01-12 17:05:32 -0500

    Slightly better soln. Removed some variables and kept the run time complexity.

--- 75 Questions/1456.py
+++ 75 Questions/1456.py
@@ -1,36 +1,37 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         vowels = {'a', 'e', 'i', 'o', 'u'}
-        left = 0
-        right = 0
         maxCount = 0
         currentCount = 0
 
         for i in range(k):
-            if right >= len(s):
+            if i >= len(s):
                 return maxCount
             
-            if s[right] in vowels:
+            if s[i] in vowels:
                 currentCount += 1
-            right += 1
-        
-        right -= 1
-        maxCount = max(maxCount, currentCount)
 
-        while right < len(s):
-            if s[left] in vowels:
-                currentCount -= 1
-            left += 1
+        maxCount = max(currentCount, maxCount)
 
-            right += 1
-            if right < len(s) and s[right] in vowels:
+        for i in range(len(s)-k):
+            if maxCount == k:
+                break
+
+            if s[i] in vowels and currentCount > 0:
+                currentCount-=1
+
+            if s[i+k] in vowels:
                 currentCount += 1
-            maxCount = max(maxCount, currentCount)
+                
+            maxCount = max(currentCount, maxCount)
 
         return maxCount
 
                 
     
 sol = Solution()
-print(sol.maxVowels("abciiidef", 3))
-print(sol.maxVowels("leetcode", 3))
+print(sol.maxVowels("tryhard", 4)) #1
+print(sol.maxVowels("tnfazcwrryitgacaabwm", 4)) #3
+print(sol.maxVowels("weallloveyou", 7)) #4
+print(sol.maxVowels("abciiidef", 3)) #3
+print(sol.maxVowels("leetcode", 3)) #2

18164e76de2e55b9f3faf778f1974cbcdd635cae
Ian McKechnie <imckechn@uoguelph.ca>
2026-01-13 15:12:50 -0500

    Considered the grid as a matrix and transposing it made it much simpler to solve.

---
+++ 75 Questions/2352.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+        found = {}
+        total = 0
+
+        for row in grid:
+            row = str(row)
+            if row in found:
+                found[row] += 1
+            else:
+                found[row] = 1
+
+        res = [[grid[j][i] for j in range(len(grid))] for i in range(len(grid[0]))]
+
+        for row in res:
+            row = str(row)
+            if row in found:
+                total += found[row]
+
+        return total
+        
+
+
+sol = Solution()
+
+print(sol.equalPairs([[3,2,1],
+                      [1,7,6],
+                      [2,7,7]]))
+
+print(sol.equalPairs([[3,1,2,2],
+                      [1,4,4,5],
+                      [2,4,2,2],
+                      [2,4,2,2]]))

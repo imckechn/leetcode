205e5e37ef4bc331c772a1c95d9037cd6192514c
Ian McKechnie <imckechn@uoguelph.ca>
2026-01-26 13:26:42 -0500

    Big chilling.

---
+++ 75 Questions/334pt2.py
@@ -0,0 +1,16 @@
+from cmath import inf
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = inf
+        second = inf
+
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        return False

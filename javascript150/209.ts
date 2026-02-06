a5fbbbc9b9d8c5f18749f8881a698557451a7282
Ian McKechnie <imckechn@uoguelph.ca>
2026-02-06 15:16:41 -0500

    Not my soln. Misread the problem as finding a sub array whose total equals the target instead of GTE, wow that would have been easier.

--- javascript150/209.ts
+++ javascript150/209.ts
@@ -1,36 +1,22 @@
 function minSubArrayLen(target: number, nums: number[]): number {
-    if (target == 0) {
-        return 0;
-    }
-    nums = nums.sort((a,b) => a-b)
-    let smallest = Infinity;
-    let left = 0, right = 0, total = nums[0];
+    let res = Number.MAX_SAFE_INTEGER;
+    let left = 0
+    let sum = 0;
 
-    while (left <= right && right < nums.length) {
-        if (total < target) {
-            right += 1
-            total += nums[right]
-        
-        } else if (total > target) {
-            total -= nums[left]
-            left += 1
-        
-        } else {
-            if (left == right) {
-                return 1
-            }
-            smallest = Math.min(smallest, right-left)
-            right += 1
+    for (let right = 0; right < nums.length; right++) {
+        sum += nums[right];
+
+        while (sum >= target) {
+            res = Math.min(res, right - left + 1);
+            left += 1;
+            sum -= nums[left - 1];
         }
     }
 
-    if (smallest == Infinity) {
-        return 0;
-    } else {
-        return smallest
-    }
+
+    return res === Number.MAX_SAFE_INTEGER ? 0 : res;
 };
 
-console.log(minSubArrayLen(11, [1,2,3,4,5]))
-console.log(minSubArrayLen(4, [1,4,4]))
+console.log(minSubArrayLen(11, [1,2,3,4,5])) //3
+console.log(minSubArrayLen(4, [1,4,4])) //1
 console.log(minSubArrayLen(7, [2,3,1,2,4,3]))

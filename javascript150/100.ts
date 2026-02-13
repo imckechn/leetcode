3ce7f36884554c1a2d7a663dfba27ab1228f5d6f
Ian McKechnie <imckechn@uoguelph.ca>
2026-02-13 16:07:31 -0500

    ez

---
+++ javascript150/100.ts
@@ -0,0 +1,25 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (!p && !q) {
+        return true
+    
+    } else if (!p && q || p && !q) {
+        return false
+    
+    } else if (p!.val != q!.val) {
+        return false
+    }
+
+    return isSameTree(p!.left, q!.left) && isSameTree(p!.right, q!.right)
+};
